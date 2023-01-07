5d777d347b1803b5ac834e9656cc2b79134e12e7
ManikumarAtche <102216676+ManikumarAtche@users.noreply.github.com>
2023-01-07 21:35:27 +0530

    def fun(n):     power=1     decimal=0     while n>0:         rem=n%10         n=n//10         decimal+=rem*power         power=power*2     return decimal n=int(input()) for i in range(n):     a=int(input())     print(fun(a))

--- Binary_to_Decimal.py
+++ Binary_to_Decimal.py
@@ -1,13 +1,13 @@
-def fun(n):#111
+def fun(n):
     power=1
     decimal=0
     while n>0:
-        rem=n%10 #1
-        n=n//10 #11
-        decimal+=rem*power#1*1
-        power=power*2#1*2
-    return decimal#1
-m=int(input())
-for i in range(m):
+        rem=n%10
+        n=n//10
+        decimal+=rem*power
+        power=power*2
+    return decimal
+n=int(input())
+for i in range(n):
     a=int(input())
     print(fun(a))
